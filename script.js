91fa5be6f82c07db5a7a734ad5e53184b9a16d33
sholtkamp <S.Kokon93@gmail.com>
2017-04-24 18:51:36 +0200

    Complete Version

--- script.js
+++ script.js
@@ -13,7 +13,14 @@ var lat1, long1, lat2, long2 = 0;
  * @source http://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
  */
 function CalculateCoords(lat1, long1, lat2, long2) {
-    alert("Fickdich!");
+
+  // Gathering data from HTML form
+   lat1  = document.getElementById("lat1").value;
+   long1 = document.getElementById("long1").value;
+   lat2  = document.getElementById("lat2").value;
+   long2 = document.getElementById("long2").value;
+
+   // Computing data by using Haversine formula specified in linked documents
     var radius = 6371; //Radius of the earth in km
     var degreeLatitude = degree2radians(lat2 - lat1);
     var degreeLongitude = degree2radians(long2 - long1);
@@ -24,18 +31,11 @@ function CalculateCoords(lat1, long1, lat2, long2) {
             ;
     var temp2 = 2 * Math.atan2(Math.sqrt(temp1), Math.sqrt(1-temp1));
     var temp3 = radius * temp2;
-    console.log(lat1, long1, lat2, long2);
-    return temp3;
+    // Changes the results paragraph in the HTML doc to display the calculated distance
+    document.getElementById("results").innerHTML ="The distance between the given coordinates is: " + temp3;
 }
 
-
+// Auxiliary function used for Haversine formula
 function degree2radians(degree){
     return degree * (Math.PI/180)
-}
-
-function submitter(){
-    lat1 = document.getElementById("lat1");
-    long1 = document.getElementById("long1");
-    lat2 = document.getElementById("lat2");
-    long2 = document.getElementById("long2");
 }
