47c1d22585d54d1c0f606fa71fb0f2714b254b07
sholtkamp <S.Kokon93@gmail.com>
2017-05-10 10:09:13 +0200

    Update script.js

--- Aufgabe 2/script.js
+++ Aufgabe 2/script.js
@@ -1,5 +1,4 @@
 var coordArray; //initializing Array
-var filecontent;
 
 /**
 * @desc reads .txt File into an Array
@@ -48,26 +47,3 @@ function Line(lat1, long1, lat2, long2){
 **/
 //------------------------------------------------------------------
 
-function TxtToArray(filecontent){
-
-	//attributes
-	var txtInput = filecontent;
-	var txtOutput = txtInput.buildArray;
-
-	// methods
-	function buildArray(input){
-		this.input = filecontent;
-		filecontent = filecontent.replace( /^\D+/g, ''); //using regex to filter all leading non-numerals
-	  filecontent = filecontent.replace( /\n/g, " " ); // using regex to change linebreaks to whitespaces
-		filecontent = filecontent.replace( /[^\d.\s]/g, ''); //using regex to filter everthing non-numeric except whitespaces
-
-		var coordArray = filecontent.split(" "); // using the whitespaces to split String into an array
-		coordArray = coordArray.filter(function(entry) { return entry.trim() != ''; }); // trimming the whitespaces from the array, after using them to split the String into the array
-		console.log(filecontent);
-		console.log(coordArray);
-
-		if(coordArray.length < 2){ // error handling for empty .txt. files
-			throw "File does not contain coordinates!";
-		  }
-	}
-}
