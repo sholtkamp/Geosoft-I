9aca6c5622396fc22401b253a42b821f4e9562eb
sholtkamp <S.Kokon93@gmail.com>
2017-05-10 08:31:23 +0200

    working regex for array generation

--- Aufgabe 2/script.js
+++ Aufgabe 2/script.js
@@ -1,4 +1,16 @@
 /**
+desired way to use filters
+
+function filterInput(content){
+	this.content = filecontent;
+	filecontent = filecontent.replace( /^\D+/g, ''); //using regex to filter all leading non-numerals
+  filecontent = filecontent.replace( /[^\d.\s]/g, ''); //using regex to filter everthing non-numeric except whitespaces
+	filecontent = filecontent.replace( /(\r\n|\n|\r)/gm,""); // using regex filter to filter all linebreaks
+}
+**/
+
+
+/**
 * @desc Create a polyline, add lines read from a given
         file and insert the full length in the HTML document
 * @see Learnweb
@@ -11,9 +23,13 @@ var ReadPolyline = function(event) {
 
   reader.onload = function(){
 	var filecontent = reader.result; //coordinate data saved to this variable
-  filecontent = filecontent.replace ( /[^\d.\s]/g, '' ); //using regex to filter everthing non-numeric except whitespaces
-	var coordArray = filecontent.split(" ");
-	coordArray = coordArray.filter(function(entry) { return entry.trim() != ''; });
+  //filecontent.filterInput(filecontent);  SO
+	filecontent = filecontent.replace( /^\D+/g, ''); //using regex to filter all leading non-numerals
+  filecontent = filecontent.replace( /[^\d.\s]/g, ''); //using regex to filter everthing non-numeric except whitespaces
+	filecontent = filecontent.replace( /(\r\n|\n|\r)/gm,""); // using regex filter to filter all linebreaks
+
+	var coordArray = filecontent.split(" "); // using the whitespaces to split String into an array
+	coordArray = coordArray.filter(function(entry) { return entry.trim() != ''; }); // trimming the whitespaces from the array, after using them to split the String into the array
 	console.log(filecontent);
 	console.log(coordArray);
 	};
