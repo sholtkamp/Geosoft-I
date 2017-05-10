3cea1f15c56c8019bbb31f3e8a6202f8e869d252
sholtkamp <S.Kokon93@gmail.com>
2017-05-10 13:07:02 +0200

    working oop arraybuilding

--- Aufgabe 2/script.js
+++ Aufgabe 2/script.js
@@ -1,4 +1,5 @@
 var coordArray; //initializing Array
+var filecontent;
 
 /**
 * @desc reads .txt File into an Array
@@ -8,42 +9,55 @@ var coordArray; //initializing Array
 var ReadFile = function(event) {
 
 	var input = event.target;
-  var reader = new FileReader();
+    var reader = new FileReader();
 
   reader.onload = function(){
-	var filecontent = reader.result; //coordinate data saved to this variable
-  //filecontent.filterInput(filecontent);  SIEHE UNTEN
-	filecontent = filecontent.replace( /^\D+/g, ''); //using regex to filter all leading non-numerals
-  filecontent = filecontent.replace( /\n/g, " " ); // using regex to change linebreaks to whitespaces
-	filecontent = filecontent.replace( /[^\d.\s]/g, ''); //using regex to filter everthing non-numeric except whitespaces
-
-	var coordArray = filecontent.split(" "); // using the whitespaces to split String into an array
-	coordArray = coordArray.filter(function(entry) { return entry.trim() != ''; }); // trimming the whitespaces from the array, after using them to split the String into the array
-	console.log(filecontent);
-	console.log(coordArray);
-
-	if(coordArray.length < 2){ // error handling for empty .txt. files
-		throw "File does not contain coordinates!";
-	  }
-
+	filecontent = reader.result; //coordinate data saved to this variable
+      myBuildArray = new BuildArray(filecontent);
+      myBuildArray.work();
 	};
 
   reader.readAsText(input.files[0]);
 };
 
 
-/**
-function Line(lat1, long1, lat2, long2){
+function BuildArray(input) {
+
+    //methods
+    this.work = function(input) {
+        filecontent = filecontent.replace(/^\D+/g, ''); //using regex to filter all leading non-numerals
+        filecontent = filecontent.replace(/\n/g, " "); // using regex to change linebreaks to whitespaces
+        filecontent = filecontent.replace(/[^\d.\s]/g, ''); //using regex to filter everthing non-numeric except whitespaces
+
+        var coordArray = filecontent.split(" "); // using the whitespaces to split String into an array
+        coordArray = coordArray.filter(function(entry) { return entry.trim() != ''; }); // trimming the whitespaces from the array, after using them to split the String into the array
+        console.log(filecontent);
+        console.log(coordArray);
+
+        if(coordArray.length < 2){ // error handling for empty .txt. files
+            throw "File does not contain coordinates!";
+        }
+
+    }
+}
+
+
+function Point(lat, long){
 
     //attributes
-    this.lat1  = coordArray[0];
-    this.long1 = coordArray[1];
-		this.lat2  = coordArray[2];
-		this.long2 = coordArray[3];
-    var length;
+    this.lat = lat;
+    this.long = long;
+}
+
+
+function Line(pt1, pt2){
+
+    //attributes
+    this.pt1  = pt1;
+    this.pt2 = pt2;
     //methods
 
   }
-**/
-//------------------------------------------------------------------
+
+
 
