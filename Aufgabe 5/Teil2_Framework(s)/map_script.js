626c16d1acf8a40c59df50dee1365333e2ad483f
sholtkamp <S.Kokon93@gmail.com>
2017-06-20 19:14:27 +0200

    map scles

--- Aufgabe 5/Teil2_Framework(s)/map_script.js
+++ Aufgabe 5/Teil2_Framework(s)/map_script.js
@@ -1,22 +1,20 @@
 //Initializing Leaflet map
-
-var map = L.map('mapid', {drawControl: true});
+var map = L.map('map', {drawControl: true});
 var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib})
 map.setView([51.961, 7.618], 13);
 map.addLayer(osm);
 
-/**
- * initilaizing augmented marker
- */
+//adding specified marker to map
 var dommarker = L.marker([51.962981, 7.625772]);
 dommarker.addTo(map);
 dommarker.bindPopup("Muenster Dom<br> Built in 13th century. <br/><img src='https://www.muenster-photos.de/cms/fileadmin/_processed_/csm_domplatz2_712f453299.jpg'/ width='100%'><br>Domplatz 28, 48143 Muenster");
 
-
 /**
  * function used to locate user
+ * this function is called from the #find_btn
+ * located the user, sets a marker at his position and opens a popup
  */
 function locateUser(){
     map.locate({setView: true, watch: true})
@@ -25,8 +23,14 @@ function locateUser(){
 
     map.addLayer(marker);
     marker.openPopup();
-})};
+})}
 
+/**
+ * function used to add external GeoJSON files to the map
+ * uses Leaflet's L.geoJson method to add data to map
+ * source is specified in the form and function called at click on the button
+ * uses JQuery Ajax to load the data
+ */
 function loadGeoJSON(){
     var exGeoJSON = new L.geoJson();
     exGeoJSON.addTo(map);
@@ -41,15 +45,18 @@ function loadGeoJSON(){
             });
         }
     }).error(function() {});
-};
+}
 
 /**
- * Funtion used for resizing map
+ * Function used for resizing map
  */
 $(map).on('map-container-resize', function () {
     map.invalidateSize(); // doesn't seem to do anything
 });
 
+/**
+ * Functions used to active and deactivate the draggable property of JQueryUI
+ */
 function moveMap() {
     $(function () {
         $("#draggable, #draggable2").draggable();
