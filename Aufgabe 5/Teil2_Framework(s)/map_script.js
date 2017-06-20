dff063cdb6794be147a0d57ecb6e7f1d929acf36
sholtkamp <S.Kokon93@gmail.com>
2017-06-20 20:36:35 +0200

    imprint, reverse, drawing

--- Aufgabe 5/Teil2_Framework(s)/map_script.js
+++ Aufgabe 5/Teil2_Framework(s)/map_script.js
@@ -1,10 +1,28 @@
 //Initializing Leaflet map
-var map = L.map('map', {drawControl: true});
+var map = L.map('map');
 var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib})
 map.setView([51.961, 7.618], 13);
 map.addLayer(osm);
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+var drawControl = new L.Control.Draw({
+    edit: {
+        featureGroup: drawnItems
+    }
+});
+map.addControl(drawControl);
+map.on(L.Draw.Event.CREATED, function (e) {
+    var type = e.layerType
+    var layer = e.layer;
+
+//insert layer to geojson
+
+    drawnItems.addLayer(layer);
+});
+
+
 
 //adding specified marker to map
 var dommarker = L.marker([51.962981, 7.625772]);
@@ -47,29 +65,83 @@ function loadGeoJSON(){
     }).error(function() {});
 }
 
-/**
- * Function used for resizing map
- */
-$(map).on('map-container-resize', function () {
-    map.invalidateSize(); // doesn't seem to do anything
-});
 
 /**
  * Functions used to active and deactivate the draggable property of JQueryUI
  */
 function moveMap() {
     $(function () {
-        $("#draggable, #draggable2").draggable();
+        $("#draggable, #draggable2").draggable({
+            disabled: false
+        });
+        $( "#map" ).resizable({
+            disabled: false,
+            alsoResizeReverse: "#draggable"
+        });
     });
 }
 
 function snapMap() {
     $(function () {
         $("#draggable, #draggable2").draggable({
-            disabled:true
+            disabled: true
+        });
+        $( "#map" ).resizable({
+            disabled: true,
+            alsoResizeReverse: false
         });
     });
 }
 
+/**
+ * "gegeinander draggen" implementation
+ * source: Simen Echholt, https://stackoverflow.com/questions/3369045/jquery-ui-resizable-alsoresize-reverse
+ */
+$.ui.plugin.add("resizable", "alsoResizeReverse", {
 
+    start: function() {
+        var that = $(this).resizable( "instance" ),
+            o = that.options;
 
+        $(o.alsoResizeReverse).each(function() {
+            var el = $(this);
+            el.data("ui-resizable-alsoresizeReverse", {
+                width: parseInt(el.width(), 10), height: parseInt(el.height(), 10),
+                left: parseInt(el.css("left"), 10), top: parseInt(el.css("top"), 10)
+            });
+        });
+    },
+
+    resize: function(event, ui) {
+        var that = $(this).resizable( "instance" ),
+            o = that.options,
+            os = that.originalSize,
+            op = that.originalPosition,
+            delta = {
+                height: (that.size.height - os.height) || 0,
+                width: (that.size.width - os.width) || 0,
+                top: (that.position.top - op.top) || 0,
+                left: (that.position.left - op.left) || 0
+            };
+
+        $(o.alsoResizeReverse).each(function() {
+            var el = $(this), start = $(this).data("ui-resizable-alsoresize-reverse"), style = {},
+                css = el.parents(ui.originalElement[0]).length ?
+                    [ "width", "height" ] :
+                    [ "width", "height", "top", "left" ];
+
+            $.each(css, function(i, prop) {
+                var sum = (start[prop] || 0) - (delta[prop] || 0);
+                if (sum && sum >= 0) {
+                    style[prop] = sum || null;
+                }
+            });
+
+            el.css(style);
+        });
+    },
+
+    stop: function() {
+        $(this).removeData("resizable-alsoresize-reverse");
+    }
+});
